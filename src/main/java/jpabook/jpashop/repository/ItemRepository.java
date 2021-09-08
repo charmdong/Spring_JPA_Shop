fdcaea45a97349c156b5b5b629c0371d06cf540d
Donggun.Chung <chungdk1117@gmail.com>
2021-09-08 23:41:00 +0900

    :hammer: 2021.09.08.Wed [make Item domain]

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        // 신규
+        if(item.getId() == null) {
+            em.persist(item);
+        }
+        // 수정
+        else {
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+}
